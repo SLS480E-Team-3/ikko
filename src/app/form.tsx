19b633542b75b90349426ad69c188935f0467c14
shuton-gif <shuton@hawaii.edu>
2026-09-25 02:01:04 -1000

    ADD DEV SignUp page

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -10,6 +10,7 @@ export default function Form() {
         <div className={styles.page}>
             <div className={styles.temp} onClick={() => router.push('./MobileTester')}>MobileTester</div>
             <div className={styles.game} onClick={() => router.push('./Game')}>GAME</div>
+            <div className={styles.game} onClick={() => router.push('./SignUp')}>SignUp</div>
         </div>
     )
 }
